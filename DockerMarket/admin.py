edb1b443755c1cfc8695aa104203dfd6796be610
shamadac <37820581+shamadac@users.noreply.github.com>
2018-03-27 20:18:28 -0400

    Add files via upload

--- DockerMarket/admin.py
+++ DockerMarket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from DockerMarket.models import User, Docker, MetagenomicSample
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Docker)
+admin.site.register(MetagenomicSample)
--- DockerMarket/models.py
+++ DockerMarket/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+# Each subclass of models.Model is associated with a table in the server DB.
+
+class MetagenomicSample(models.Model):
+    # Model for metagenomic sample data.
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=110, unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(MetagenomicSample, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+        
+
+class Docker(models.Model):
+    # Model for Docker data.
+    title = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=110, unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(MetagenomicSample, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.title
+        
+
+class User(models.Model):
+    # Model for user data.
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    email = models.CharField(max_length=75)
+    
+    def __str__(self):
+        fullName = str(self.firstName, self.lastName)
+        return fullName
